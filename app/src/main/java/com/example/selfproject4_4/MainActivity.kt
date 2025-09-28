c714b53335f6154c8c9d758c96dcc89bb21faa1c
kimhonggyeong <hk010626@gmail.com>
2025-09-28 15:33:51 +0900

    과제 업데이트

--- app/src/main/java/com/example/selfproject4_4/MainActivity.kt
+++ app/src/main/java/com/example/selfproject4_4/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
